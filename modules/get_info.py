3acfc76a07bca2b7a1696e0a51ebbcb754ab6285
MarcosPimienta <fenix3819@gmail.com>
2023-08-05 22:32:24 -0500

    added exceptions

--- modules/get_info.py
+++ modules/get_info.py
@@ -1,7 +1,9 @@
-import requests
-
-
 def get_weather(api_key, city):
+
+    if not api_key or not city:
+        print("API key and city name must not be empty.")
+        return
+
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     params = {
         'q': city,
@@ -10,19 +12,18 @@ def get_weather(api_key, city):
 
     try:
         response = requests.get(base_url, params=params)
-        response.raise_for_status()  # If the request was not successful
+        response.raise_for_status()
+        data = response.json()
+
+        city_name = data["name"]
+        temperature = data["main"]["temp"] - 273.15
+        print(f"The current temperature in {city_name} is {temperature:.2f}°C")
 
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
-        return None
     except requests.exceptions.RequestException as err:
         print(f"An error occurred: {err}")
-        return None
-
-    data = response.json()
-
-    # Now you can extract and use the data from the response
-    # For example, print the name of the city and the current temperature:
-    city_name = data["name"]
-    temperature = data["main"]["temp"] - 273.15
-    print(f"The current temperature in {city_name} is {temperature:.2f}°C")
+    except ValueError:
+        print("Decoding JSON has failed")
+    except KeyError:
+        print("Invalid city name. Please check your spelling and try again.")
